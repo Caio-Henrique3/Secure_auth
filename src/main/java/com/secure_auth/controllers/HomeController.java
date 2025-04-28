4b276b3a82b435ea0cb5b649a9c8edd7cbf8a7a5
Caio-Henrique3 <caioaranha123@hotmail.com>
2025-04-28 16:26:55 -0300

    refactor: flow reconstruction using authentication through azure

--- src/main/java/com/secure_auth/controllers/HomeController.java
+++ src/main/java/com/secure_auth/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package com.secure_auth.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HomeController {
@@ -9,6 +13,15 @@ public class HomeController {
     @GetMapping
     public String hello() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/me")
+    public Map<String, Object> me(@AuthenticationPrincipal OidcUser oidcUser) {
+        return Map.of(
+                "name", oidcUser.getFullName(),
+                "email", oidcUser.getEmail(),
+                "id", oidcUser.getSubject()
+        );
     }
 
 }
